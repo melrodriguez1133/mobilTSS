90e5fed04848adac7971cb7a629d0b8b8328ddb7
melrodriguez1133 <202000231@est.umss.edu>
2023-12-01 00:04:37 -0400

    menu pulido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
+import { DatosComponent } from './datos/datos.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '/home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'home',
+    component: HomeComponent 
+  },
+  {
+    path: 'datos',
+    component: DatosComponent 
+  },
+  {
+    path: 'ayuda',
+    component: AyudaComponent 
+  },
+  {
+    path: '**', 
+    redirectTo: '/home'
   }
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
+import { DatosComponent } from './datos/datos.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+   HomeComponent,
+   AyudaComponent,
+   DatosComponent
+  ],
+  imports: [
+    BrowserModule,
+     IonicModule.forRoot(),
+      AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
