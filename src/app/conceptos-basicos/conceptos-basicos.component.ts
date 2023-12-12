2fffee846b42dc714f9e7e54e7d04e61e1edc99a
melrodriguez1133 <202000231@est.umss.edu>
2023-12-11 23:13:19 -0400

    adicion de botones y arreglo de acentos

--- src/app/conceptos-basicos/conceptos-basicos.component.ts
+++ src/app/conceptos-basicos/conceptos-basicos.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conceptos-basicos',
   templateUrl: './conceptos-basicos.component.html',
   styleUrls: ['./conceptos-basicos.component.scss'],
 })
-export class ConceptosBasicosComponent  implements OnInit {
+export class ConceptosBasicosComponent {
 
-  constructor() { }
+  constructor(private router: Router) {
+    // Constructor del componente
+  }
 
-  ngOnInit() {}
-
+  volverAlMenu() {
+    this.router.navigate(['/ayuda']);
+  }
+  volversimulacion() {
+    this.router.navigate(['/datos']);
+  }
 }
